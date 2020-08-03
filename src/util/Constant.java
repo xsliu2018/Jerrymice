130ded1c4874a5217ed1bd60d7dd4f9f316db5f8
xsliu <xsl2011@outlook.com>
2020-08-03 22:36:39 +0800

    完成request和response两个类

---
+++ src/util/Constant.java
@@ -0,0 +1,11 @@
+package util;
+
+/**
+ * @author ：xiaosong
+ * @description：TODO
+ * @date ：2020/8/3 21:43
+ */
+public class Constant {
+    public final static String responseHead200 = "HTTP/1.1 200 OK\r\n" +
+            "Content-Type: {}\r\n\r\n";
+}
